64753aae08b269d6d32ffa31f1e9755f7cd40696
daili0015 <daili0015@gmail.com>
2019-02-16 11:33:55 +0800

    c1wideresnet

--- models/StereoCNN/c1wideresnet.py
+++ models/StereoCNN/c1wideresnet.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # @Author: zcy
-# @Date:   2019-02-14 19:29:27
+# @Date:   2019-02-16 11:06:01
 # @Last Modified by:   zcy
-# @Last Modified time: 2019-02-16 11:18:59
+# @Last Modified time: 2019-02-16 11:33:27
+
+# 1 channel in; bigger expansion
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
 from functools import partial
 
-__all__ = ['WideResNet', 'WideBottleneck']
+__all__ = ['c1wideresnet50_3d']
 
 
 def conv3x3x3(in_planes, out_planes, stride=1):
@@ -38,7 +41,7 @@ def downsample_basic_block(x, planes, stride):
 
 
 class WideBottleneck(nn.Module):
-    expansion = 2
+    expansion = 8
 
     def __init__(self, inplanes, planes, stride=1, downsample=None):
         super(WideBottleneck, self).__init__()
@@ -82,16 +85,16 @@ class WideResNet(nn.Module):
     def __init__(self,
                  block,
                  layers,
-                 k=1,
+                 k=2,
                  shortcut_type='B',
-                 n_classes=400,
-                 in_channels=3):
+                 n_classes=2,
+                 in_channels=1):
 
         super(WideResNet, self).__init__()
 
-        first_features = 64 if in_channels==3 else 32
+        first_features = 16
         self.inplanes = first_features
-
+        # 1->16
         self.conv1 = nn.Conv3d(
             in_channels,
             first_features,
@@ -102,15 +105,17 @@ class WideResNet(nn.Module):
         self.bn1 = nn.BatchNorm3d(first_features)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool3d(kernel_size=(3, 3, 3), stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64 * k, layers[0], shortcut_type)
+        base = first_features
+        nStages = [base*k, base*2*k, base*4*k, base*8*k]
+        self.layer1 = self._make_layer(block, nStages[0], layers[0], shortcut_type)
         self.layer2 = self._make_layer(
-            block, 128 * k, layers[1], shortcut_type, stride=2)
+            block, nStages[1], layers[1], shortcut_type, stride=2)
         self.layer3 = self._make_layer(
-            block, 256 * k, layers[2], shortcut_type, stride=2)
+            block, nStages[2], layers[2], shortcut_type, stride=2)
         self.layer4 = self._make_layer(
-            block, 512 * k, layers[3], shortcut_type, stride=2)
+            block, nStages[3], layers[3], shortcut_type, stride=2)
 
-        self.fc = nn.Linear(512 * k * block.expansion, n_classes)
+        self.fc = nn.Linear( nStages[3] * block.expansion, n_classes)
 
         for m in self.modules():
             if isinstance(m, nn.Conv3d):
@@ -150,12 +155,16 @@ class WideResNet(nn.Module):
         x = self.relu(x)
         x = self.maxpool(x)
 
+        # print(x.shape)
         x = self.layer1(x)
+        # print(x.shape)
         x = self.layer2(x)
+        # print(x.shape)
         x = self.layer3(x)
         x = self.layer4(x)
 
         # x = self.avgpool(x)
+        # print(x.shape)
         x = F.adaptive_avg_pool3d(x, (1, 1, 1))
 
         x = x.view(x.size(0), -1)
@@ -163,19 +172,34 @@ class WideResNet(nn.Module):
 
         return x
 
-def wideresnet50_3d(**kwargs):
+
+def c1wideresnet18_3d(**kwargs):
+    """Constructs a ResNet-50 model.
+    """
+    model = WideResNet(WideBottleneck, [2, 2, 2, 2], **kwargs)
+    return model
+
+def c1wideresnet50_3d(**kwargs):
     """Constructs a ResNet-50 model.
     """
     model = WideResNet(WideBottleneck, [3, 4, 6, 3], **kwargs)
     return model
 
+def c1wideresnet101_3d(**kwargs):
+    """Constructs a ResNet-50 model.
+    """
+    model = WideResNet(WideBottleneck, [3, 4, 23, 3], **kwargs)
+    return model
+
+
+
+
 if __name__ == '__main__':
     a = 64
     img_size=(a, a)
-    model = wideresnet50_3d(n_classes=2, in_channels=1)
+    model = c1wideresnet50_3d(n_classes=2, in_channels=1)
     x = torch.randn(3, 1, 22, img_size[0], img_size[1])
     # (BatchSize, channels, depth, h, w)
     y = model(x)
     torch.save(model.state_dict(), "m.pth")
     print(y.size())
-

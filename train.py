e73008e574396f905fd4f0475a31226871ee7020
zcy <zcy0016@163.com>
2019-03-05 13:02:59 +0800

    s

--- train.py
+++ train.py
@@ -9,12 +9,14 @@
 
 from classifier import classifier
 
+# if __name__ == '__main__':
+#     clf = classifier('xception', 17, (60, 60), '/home/TensorFlow/keras/flowers17-tsycnh package/flowers17/train')
+#     clf.set_trainer(epochs=1, save_dir="saved/", save_period=1, verbosity=2, 
+#         verbose_per_epoch=100, monitor = "max val_accuracy", early_stop=10, 
+#         tensorboardX=False, log_dir="saved/runs", steps_update=1)
+#     clf.train()
+
 if __name__ == '__main__':
-    clf = classifier('xception', 17, (60, 60), 'E:/Oxford_Flowers17/train')
-    clf.set_trainer(epochs=1, save_dir="saved/", save_period=1, verbosity=2, 
-        verbose_per_epoch=100, monitor = "max val_accuracy", early_stop=10, 
-        tensorboardX=False, log_dir="saved/runs", steps_update=1)
-    clf.train()
-
-
+    clf = classifier('xception', 17, (60, 60), '/home/TensorFlow/keras/flowers17-tsycnh package/flowers17/train')
+    clf.train_from('/home/DL/ModelFeast/saved/xception/0305_125940/checkpoint_best.pth')
 
